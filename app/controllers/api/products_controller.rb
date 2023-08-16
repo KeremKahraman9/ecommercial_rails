4a819e056c6402116e5c210391ef9e3c9b73e90c
Kerem Kahraman <kerem.kahraman001@gmail.com>
2023-08-16 22:52:43 +0300

    product controller improvements

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,8 +1,30 @@
 module Api
   class ProductsController < ApplicationController
     
+    def index
+      @products = Product.all
+      render json: @products
+    end
+
+    def show
+      @products = Product.find(params[:id])
+      render json: @products
+    end
+
     def create
-      p 'Create is working'
+      @product = Product.create(product_params)
+      @product.save
+      render json: @products
+    end
+
+    def update
+      @product = Product.find(params[:id])
+      @product = update(product_params)
+      render json: @products
+    end
+
+    def product_params
+      params.permit(:name, :description, :quantity, :price)
     end
 
   end
