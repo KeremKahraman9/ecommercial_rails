5269864471cb46d0c64749677aa8efb1fd9cf061
Kerem Kahraman <kerem.kahraman001@gmail.com>
2023-08-17 22:41:06 +0300

    rails active storage added

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,9 +1,61 @@
 module Api
   class CategoriesController < ApplicationController
-    
+    protect_from_forgery with: :null_session
+
+    def index
+      @categories = Category.order(created_at: :desc)
+      if !@categories.blank?
+        render json: @categories, status: :ok
+      else
+        render json: @categories, status: :bad_request
+      end
+    end
+
+    def show
+      category = set_category
+      if !category.blank?
+        render json: @categories, status: :ok
+      else
+        render json: @categories, status: :bad_request
+      end
+    end
+
     def create
-      p 'Create is working'
+      @category = Category.create(params_category)
+      if @category.save
+        render json: @category, status: :ok
+      else
+        render json: "Create Failed", status: :bad_request
+      end
+    end
+
+    def update
+      category = set_category
+      if category.update(params_category)
+        render json: @category, status: :ok
+      else
+        render json: "Update Failed", status: :bad_request
+      end
+    end
+
+    def destroy
+      category = set_category
+      if category.destroy
+        render json: "Delete Success", status: :ok
+      else
+        render json: "Delete Failed", status: :bad_request
+      end
     end
     
+    private
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def params_category
+      params.permit(:name)
+    end
+
   end
 end
--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,14 +1,16 @@
 module Api
   class ProductsController < ApplicationController
-    
+    protect_from_forgery with: :null_session
+
     def index
       @products = Product.all
       render json: @products
     end
 
     def show
-      @products = Product.find(params[:id])
-      render json: @products
+      @product = Product.find(params["id"])
+      image = rails_blob_url(@product.product_image)
+      render json: { "image": image, "data": @product }
     end
 
     def create
@@ -18,13 +20,19 @@ module Api
     end
 
     def update
-      @product = Product.find(params[:id])
+      @product = Product.find(params["id"])
       @product = update(product_params)
       render json: @products
     end
 
+    def destroy
+      @product = Product.find(params["id"])
+      @product.destroy
+      render json: "#{@product.id} id'li kayıt silindi"
+    end
+
     def product_params
-      params.permit(:name, :description, :quantity, :price)
+      params.permit(:name, :description, :quantity, :price, :product_image)
     end
 
   end
