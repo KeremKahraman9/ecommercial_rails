11fc1742018c74bac3201a235f46ee1776010dbf
Kerem Kahraman <kerem.kahraman001@gmail.com>
2023-08-21 23:45:33 +0300

    pundit policies added for models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   skip_before_action :verify_authenticity_token
-  before_action :configure_permited_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  def configure_permited_parameters
+  def user_not_authorized
+    @message = "You have no auth"
+    render json: {message: @message}, status:401
+  end
+
+  def configure_permitted_parameters
+    byebug
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :email, :password, :password_confirmation])
   end
 end
